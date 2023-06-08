a1d564e4a308c1deec1f7deffe23f3422f66d83f
ajay09a <92861422+ajay09a@users.noreply.github.com>
2023-06-09 00:13:55 +0530

    footer created and update with functions

---
+++ src/components/Footer.js
@@ -0,0 +1,69 @@
+import React from 'react'
+import { NavLink } from "react-router-dom";
+
+const Footer = () => {
+  return (
+    <div>
+        <div className='details-and-links'>
+            <div className='address'>
+                <img src='' alt='logo' />
+                <p>101 Relcon Enclave, NH-48</p>
+                <p>Surat, Gujarat</p>
+                <p>India - 394325</p>
+            </div>
+            <div className='company'>
+                <h2>Company</h2>
+                <ul>
+                    <li>
+                        <NavLink to='/about'>About Us</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to='/contactus'>Contact Us</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to='/pricing'>Pricing</NavLink>
+                    </li>
+                </ul>
+            </div>
+            <div className='quick-links'>
+                <h2>Quick Links</h2>
+                <ul>
+                    <li>
+                        <NavLink to='/policy/guidelines'>Community Guidelines</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to='/policy'>Privacy Policy</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to='/policy/terms'>Term of uses</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to='/policy/cancellation'>Cancellation & Refund</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to='/policy/shipping'>Shipping & Exchange</NavLink>
+                    </li>
+                </ul>
+            </div>
+            <div className='faqs'>
+                <h2>FAQs</h2>
+                <ul>
+                    <li>
+                        <NavLink to='/faq'>Startup FAQs</NavLink>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <div className='social-link-and-rights'>
+            <div className='social-link'></div>
+            <div className='rights'>
+                <p>&#169;</p>
+                <NavLink to='/'>StartupzWorld,</NavLink>
+                <p>All Rights Reserved</p>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Footer
