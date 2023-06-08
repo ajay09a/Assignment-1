399fb3bb3006a037f81b25ba087342f903532a1a
ajay09a <92861422+ajay09a@users.noreply.github.com>
2023-06-09 03:08:54 +0530

    add login page and style

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,22 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 
 const Login = () => {
   return (
     <div id='login'>
-        
+        <div className='heading'>
+            <h1>Startup</h1>
+            <p>Login</p>
+        </div>
+        <form>
+            <label>Username</label>
+            <input placeholder='Enter Mobile or Email' />
+            <button>Log in</button>
+        </form>
+        <div className='register'>
+            <p>New User? Please </p>
+            <NavLink to='/register'>Register</NavLink>
+        </div>
     </div>
   )
 }
