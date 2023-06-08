c7981d688d99f4fe7a580683d6e80baa2d9e66ba
ajay09a <92861422+ajay09a@users.noreply.github.com>
2023-06-08 23:50:50 +0530

    Home created

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Navbar from './components/Navbar'
+import Home from './components/Home';
 
 const App = () => {
   return (
@@ -8,7 +9,7 @@ const App = () => {
       <Router>
         <Navbar />
         <Routes>
-          {/* <Route path='/' element={<Home />} ></Route> */}
+          <Route path='/' element={<Home />} ></Route>
         </Routes>
       </Router>
     </div>
