13e2d0a1534518f331a5db9b959ea54096fb7b66
ajay09a <92861422+ajay09a@users.noreply.github.com>
2023-06-09 08:49:28 +0530

    create and style investor page

--- src/pages/Investor.js
+++ src/pages/Investor.js
@@ -6,26 +6,26 @@ const Investor = () => {
         <h1>Investor</h1>
         <p>Welcome to our platform, where we connect innovative startups with savvy investors. Our goal is to make the investment process as seamless and efficient as possible. We understand that as an investor, you are looking for opportunities to grow your portfolio and generate returns. That's why we've created a platform that streamlines the process of finding, researching, and investing in startups.</p>
         <p>Our platform offers a wide range of startups across various industries and stages of development, providing investors with a diverse range of investment opportunities.</p>
-        <div><img src='' alt='tick' /><p>A curated selection of high-potential startups</p></div>
-        <div><img src='' alt='tick' /><p>Detailed information on each startup, including financials and pitch decks</p></div>
-        <div><img src='' alt='tick' /><p>A streamlined investment process</p></div>
-        <div><img src='' alt='tick' /><p>Exclusive events and networking opportunities</p></div>
-        <div><img src='' alt='tick' /><p>Wide range of investment options across various industries and sectors</p></div>
-        <div><img src='' alt='tick' /><p>Access to vetted and un-vetted startups</p></div>
-        <div><img src='' alt='tick' /><p>Transparent and user-friendly interface</p></div>
-        <div><img src='' alt='tick' /><p>Opportunity to connect and network with other like-minded individuals</p></div>
-        <div><img src='' alt='tick' /><p>Dedicated team of experts constantly researching and analyzing the startup ecosystem</p></div>
-        <div><img src='' alt='tick' /><p>Access to regular reports, newsletters and webinars on the latest trends and developments in the startup ecosystem.</p></div>
+        <div className='check'><img src='https://cdn-icons-png.flaticon.com/512/5290/5290982.png' alt='tick' /><p>A curated selection of high-potential startups</p></div>
+        <div className='check'><img src='https://cdn-icons-png.flaticon.com/512/5290/5290982.png' alt='tick' /><p>Detailed information on each startup, including financials and pitch decks</p></div>
+        <div className='check'><img src='https://cdn-icons-png.flaticon.com/512/5290/5290982.png' alt='tick' /><p>A streamlined investment process</p></div>
+        <div className='check'><img src='https://cdn-icons-png.flaticon.com/512/5290/5290982.png' alt='tick' /><p>Exclusive events and networking opportunities</p></div>
+        <div className='check'><img src='https://cdn-icons-png.flaticon.com/512/5290/5290982.png' alt='tick' /><p>Wide range of investment options across various industries and sectors</p></div>
+        <div className='check'><img src='https://cdn-icons-png.flaticon.com/512/5290/5290982.png' alt='tick' /><p>Access to vetted and un-vetted startups</p></div>
+        <div className='check'><img src='https://cdn-icons-png.flaticon.com/512/5290/5290982.png' alt='tick' /><p>Transparent and user-friendly interface</p></div>
+        <div className='check'><img src='https://cdn-icons-png.flaticon.com/512/5290/5290982.png' alt='tick' /><p>Opportunity to connect and network with other like-minded individuals</p></div>
+        <div className='check'><img src='https://cdn-icons-png.flaticon.com/512/5290/5290982.png' alt='tick' /><p>Dedicated team of experts constantly researching and analyzing the startup ecosystem</p></div>
+        <div className='check'><img src='https://cdn-icons-png.flaticon.com/512/5290/5290982.png' alt='tick' /><p>Access to regular reports, newsletters and webinars on the latest trends and developments in the startup ecosystem.</p></div>
         <p>By joining our platform, you'll have access to a diverse range of investment opportunities and the support you need to make informed decisions. Let's work together to build your portfolio and achieve your financial goals.</p>
         <hr />
-        <div>
+        <div className='details'>
             <div>
-                <span>9050656</span>
-                <h3>Investor</h3>
+                <span>90,50,656</span>
+                <h4>Investor</h4>
             </div>
             <div>
-                <span>20956737</span>
-                <h3>Registed Startups</h3>
+                <span>2,09,56,737</span>
+                <h4>Registed Startups</h4>
             </div>
         </div>
         <hr />
